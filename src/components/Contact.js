ca5a8ca59ddba97ac43d2155ff4050efa65f089c
anapy <anabelenfunesc@gmail.com>
2020-08-17 20:38:11 +0200

    adding some content to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,35 @@ import '../stylesheets/contact.scss';
 
 const Contact = props => {
 
-  return <div className="contact">
-
-  </div>;
+  return (
+    <div class="contact">
+      <h2 class="contact__title">Contacto</h2>
+      <div class="contact__paragraph">
+        <p class="contact__paragraph--first">
+          No lo dudes y envíanos tus comentarios, dudas, preguntas...¡Estaremos
+          encantadas de responderte!
+        </p>
+      </div>
+      <form action="https://formspree.io/xeqrkebj" method="post" class="contact__form">
+        <div class="contact__form--group">
+          <label for="fullName">Nombre <span class="contact__star"> *</span></label>
+          <input type="text" id="fullName" name="fullName" placeholder="Nombre..." required />
+        </div>
+        <div class="contact__form--group email-tel">
+          <div class="contact__form--email">
+            <label for="email">Email<span class="contact__star"> *</span></label><input type="email" id="email"
+              name="email" placeholder="nombre.apellidos@mail.com" required />
+          </div>
+        </div>
+        <div class="contact__form--group">
+          <label for="message">Mensaje<span class="contact__star"> *</span>
+            <textarea class="contact__form--textarea" id="message" name="message" placeholder="¿Qué necesitas?"
+              required></textarea>
+          </label>
+        </div>
+        <input class="contact__form--button" type="submit" value="Enviar" />
+      </form>
+  </div>);
 }
 
 export default Contact;
