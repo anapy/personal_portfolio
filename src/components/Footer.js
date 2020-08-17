8496f6a9ea25424ab3e47fcb0b155a935b226ec6
anapy <anabelenfunesc@gmail.com>
2020-08-17 20:17:10 +0200

    adding mouse over and mouse out events

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,14 @@ library.add(fab, fas)
 
 const Contact = props => {
   const [hide, setHide] = useState('');
-  const clickHandler = ev =>{
-    ev.preventDefault();
-    hide !== '' ? setHide('') : setHide('open');
+  const mouseOverHandler = ev =>{
+    setHide('open');
   }
+
+  const mouseOutHandler = ev =>{
+    setHide('');
+  }
+
   return <footer className="footer">
   <nav className="circular-menu">
     <div className={`circle ${hide !== '' ? 'open' : ''}`}>
@@ -20,7 +24,7 @@ const Contact = props => {
         <a href="https://github.com/anapy" className="menu-item github"> <FontAwesomeIcon icon={['fab', 'github']}/> </a>
         <a href="https://twitter.com/anajapy" className="menu-item twitter"> <FontAwesomeIcon icon={['fab', 'twitter']}/></a>
     </div>
-    <button className="menu-button" onClick={clickHandler}><FontAwesomeIcon className="share_logo" icon={['fas', 'share-alt']}/></button>
+    <button className="menu-button" onMouseOver={mouseOverHandler} onMouseOut={mouseOutHandler}><FontAwesomeIcon className="share_logo" icon={['fas', 'share-alt']}/></button>
   </nav>
   </footer>;
 }
