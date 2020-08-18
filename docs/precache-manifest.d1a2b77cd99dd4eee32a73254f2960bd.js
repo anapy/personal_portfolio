0e42159ae083e5cad3b27896fa1e381cbf75f4f1
anapy <anabelenfunesc@gmail.com>
2020-08-18 11:12:50 +0200

    updating docs

---
+++ docs/precache-manifest.d1a2b77cd99dd4eee32a73254f2960bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe4abc69d41b60b1cf29eae41a94e27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a24ba7d28114690607e",
+    "url": "./static/css/main.03be80b6.chunk.css"
+  },
+  {
+    "revision": "63f6f12136f86e277770",
+    "url": "./static/js/2.26ab174a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.26ab174a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a24ba7d28114690607e",
+    "url": "./static/js/main.0a7cc440.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  }
+]);
