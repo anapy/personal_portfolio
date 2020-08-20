1be95edd0a3a94522774198d530f9170d50f0046
anapy <anabelenfunesc@gmail.com>
2020-08-20 12:11:20 +0200

    adding mediaqueries to aboutMe component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,21 +10,21 @@ const AboutMe = () => {
       <div className="img_home_container"><img src="./images/AB_black.jpg" alt="foto" height="300px"></img></div>
       <div className="article_container">
       <article className="studies">
-        <h3 className="article_aboutMe_title">Formación Académica</h3>
+        <h3 className="article_title">Formación Académica</h3>
         <p>2020 - Curso en Adalab de <b>Programación Front-End</b></p>
         <p>2019 - Inicio grado en <b>Ingeniería Informática</b></p>
         <p>2016 - Master en <b>Comercio Internacional</b></p>
         <p> 2015 - Graduada en <b>Traducción e Interpretación</b></p>
       </article>
       <article className="tecnologies">
-        <h3 className="article_aboutMe_title">Conocimientos en tecnologías</h3>
+        <h3 className="article_title">Conocimientos en tecnologías</h3>
         <p>En el curso de programación Front-End he desarrollado proyectos usando tecnologías como
           HTML, CSS, SASS, Javascript y React. También he usado Git para el control de versiones y la filosofía agile como marco de trabajo. </p>
         <p>Durante la carrera de ingenería aprendí los fundamentos de la programación usando el lenguaje C. Posteriormente, he aprendido Java y las bases de la Programación Orientada a Objetos.</p>
         <p>Por mi cuenta, he aprendido Python, con el curso de <i>Python for everybody</i> y actualmente estoy aprendiendo a crear una API Rest y el lenguaje de consulta de base de datos SQL.</p>
       </article>
       <article className="hobbies">
-        <h3 className="article_aboutMe_title">Idiomas</h3>
+        <h3 className="article_title">Idiomas</h3>
         <p>Inglés - C1</p>
         <p>Alemán - B2</p>
       </article>
