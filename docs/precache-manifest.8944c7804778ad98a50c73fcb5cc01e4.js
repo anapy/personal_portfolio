f82676840823efc4653334c381e1374c0147e017
anapy <anabelenfunesc@gmail.com>
2024-05-11 18:10:23 +0200

    update build and photo

---
+++ docs/precache-manifest.8944c7804778ad98a50c73fcb5cc01e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8f6cbbcdb34d5ca29c5492ff9a04e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33feacc77df2c54c74c9",
+    "url": "./static/css/main.e758182c.chunk.css"
+  },
+  {
+    "revision": "8c5c07d0506838297197",
+    "url": "./static/js/2.d2a8203e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d2a8203e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33feacc77df2c54c74c9",
+    "url": "./static/js/main.9b36289e.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  }
+]);
